351c5fe4b32f4ea5e30e6c55d8d660cb8cd7e518
Sujitha_Sivakumar <sujithasiva161@gmail.com>
2026-03-27 16:22:18 +0530

    Random data generation for 2D and 3D arrays

--- Array/Packed Array.sv
+++ Array/Packed Array.sv
@@ -7,15 +7,15 @@ module tb;
     data=$random;
     $display("data=%h %b",data,data);
     for (int  i=0;i<$size(data);i++) begin
-      $display("data[%d]=%b (%h)",i,data[i],data[i]);
+      $display("data[%0d]=%b [%h]",i,data[i],data[i]);
     end 
     //3D
     n_data=$random;
     $display("n_data=%b",n_data);
     foreach(n_data[i])begin
-      $display("n_data[%d]=%b",i,n_data[i]);
+      $display("n_data[%0d]=%b",i,n_data[i]);
       foreach(n_data[i][j])begin
-        $display("n_data[%d ,%d]=%b",i,j,n_data[i][j]);
+        $display("n_data[%0d][%0d]=%b",i,j,n_data[i][j]);
       end
     end
   end
